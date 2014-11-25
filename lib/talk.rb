7cb097ac2b954bd04d43ad011f8c24f43a964ef7
Andrew Braithwaite <andrew.braithwaite@laterooms.com>
2014-11-25 15:13:52 +0000

    refactor

--- lib/talk.rb
+++ lib/talk.rb
@@ -3,13 +3,15 @@ class Talk
   attr_reader :description
   attr_reader :length
 
+  LIGHTNING_TALK_LENGTH = 5
+
   def initialize(talk)
     words = talk.split
 
     @description = talk[/.*(?=\s)/]
 
     if words.last == 'lightning'
-      @length = 5
+      @length = LIGHTNING_TALK_LENGTH
     elsif /(\d+)min/.match(words.last)
       @length = /(\d+)min/.match(words.last).captures[0].to_i
     else

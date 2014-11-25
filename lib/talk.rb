0c253a3e866f886a3aa9af163c684eae3e841960
Andrew Braithwaite <andrew.braithwaite@laterooms.com>
2014-11-25 14:48:48 +0000

    handle talk passed in as string with length in minutes

--- lib/talk.rb
+++ lib/talk.rb
@@ -3,8 +3,8 @@ class Talk
   attr_reader :description
   attr_reader :length
 
-  def initialize(description, length)
-    @description = description
-    @length = length
+  def initialize(talk)
+    @length = /(\d+)min/.match(talk).captures[0].to_i
+    @description = talk[/.*(?=\s)/]
   end
 end
--- spec/talk_spec.rb
+++ spec/talk_spec.rb
@@ -4,7 +4,7 @@ require 'spec_helper'
 describe 'talk' do
   it 'populates description and length correctly with length specified'\
      ' in minutes' do
-    talk = Talk.new('this is a talk', 45)
+    talk = Talk.new('this is a talk 45min')
 
     expect(talk.description).to eq('this is a talk')
     expect(talk.length). to eq(45)

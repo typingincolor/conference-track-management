0ec650bee035a605135845b110376419effb4d1a
Andrew Braithwaite <andrew.braithwaite@laterooms.com>
2014-11-28 15:31:19 +0000

    only include lib directory in code coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,9 @@
 require 'simplecov'
-SimpleCov.start
+SimpleCov.start do
+  add_filter do |src|
+    !(src.filename =~ /^#{SimpleCov.root}\/lib/)
+  end
+end
 
 require_relative '../lib/talk'
 require_relative '../lib/session'

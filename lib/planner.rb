b19e9fcd57aff3940b484257ae2c165897eeb532
Andrew Braithwaite <abraithw@gmail.com>
2014-11-28 22:49:52 +0000

    refactor

--- lib/planner.rb
+++ lib/planner.rb
@@ -2,12 +2,13 @@
 class Planner
   def plan(talks, session_lengths)
     sessions = setup_sessions(session_lengths)
-
-    iterate_over_sessions(sessions, talks)
+    processed = Array.new talks.length, false
+    iterate_over_sessions(sessions, talks, processed)
   end
 
   def setup_sessions(session_lengths)
     sessions = []
+
     session_lengths.each do |session_length|
       sessions.push(Session.new session_length)
     end
@@ -15,8 +16,7 @@ class Planner
     sessions
   end
 
-  def iterate_over_sessions(sessions, talks)
-    processed = Array.new talks.length, false
+  def iterate_over_sessions(sessions, talks, processed)
     sessions.each do |session|
       talks.each_with_index do |talk, index|
         next if processed[index]
@@ -29,5 +29,5 @@ class Planner
     sessions
   end
 
-  private :setup_sessions
+  private :setup_sessions, :iterate_over_sessions
 end

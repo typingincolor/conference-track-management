a3f4cb2b5dac71463f27e6a43fbc4fe86f42755f
Andrew Braithwaite <andrew.braithwaite@laterooms.com>
2014-11-28 14:53:22 +0000

    create talk list from a file

--- lib/talk_list.rb
+++ lib/talk_list.rb
@@ -1,3 +1,16 @@
+require 'talk'
+
 # Talk List
 class TalkList < Array
+  def initialize(filename = '')
+    File.readlines(filename).each do |line|
+      push Talk.new(remove_newline line)
+    end unless filename == ''
+  end
+
+  def remove_newline(str)
+    str.gsub(/\n/, '')
+  end
+
+  private :remove_newline
 end
--- spec/talk_list_spec.rb
+++ spec/talk_list_spec.rb
@@ -21,4 +21,22 @@ describe 'talk' do
     expect(list[0]).to eq Talk.new('a talk 60min')
     expect(list[1]).to eq Talk.new('a talk 45min')
   end
+
+  it 'reads a talk list from a file' do
+    list = TalkList.new './spec/test_list.txt'
+
+    expect(list.size).to eq 5
+
+    expect(list[0].description).to eq 'Talk one'
+    expect(list[1].description).to eq 'Talk two'
+    expect(list[2].description).to eq 'Talk three'
+    expect(list[3].description).to eq 'Talk four'
+    expect(list[4].description).to eq 'Talk five'
+
+    expect(list[0].length).to eq 10
+    expect(list[1].length).to eq 25
+    expect(list[2].length).to eq 5
+    expect(list[3].length).to eq 10
+    expect(list[4].length).to eq 15
+  end
 end
